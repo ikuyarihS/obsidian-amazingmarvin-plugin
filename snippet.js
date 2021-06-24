c7a21e56c07cce187c3a8df538a762d855890af7
Shirayuki Nekomata <lethienphuoc1991@gmail.com>
2021-06-24 20:44:09 +0700

    Remove snippet.js
    Accidently committed

--- snippet.js
+++
@@ -1,9 +0,0 @@
-const hyperlinkRegex = /\[(?<text>[^\]]+?)\]\((?<href>https?:\S+?)\)/g;
-const text =
-  'Get a broad overview - ';
-
-let link;
-while ((link = hyperlinkRegex.exec(text))) {
-  const [_, x, href] = [...link];
-  console.log(x, href);
-}
