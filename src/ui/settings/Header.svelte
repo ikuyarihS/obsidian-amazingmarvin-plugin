34e4a978e3684e9a819ab0b49aaff03ad4ca9108
Shirayuki Nekomata <lethienphuoc1991@gmail.com>
2021-07-05 21:02:18 +0700

    Move settings out of main.ts

---
+++ src/ui/settings/Header.svelte
@@ -0,0 +1,9 @@
+<div class="amazing-marvin-setting-header">
+  <h2>Amazing Marvin Plugin Settings</h2>
+  <p>Configure API tokens to interact with Amazing Marvin API.</p>
+  <span>
+    For more information, you can read more on <a href="https://github.com/amazingmarvin/MarvinAPI/wiki/Marvin-API">
+      Amazing Marvin API Github
+    </a>
+  </span>
+</div>
